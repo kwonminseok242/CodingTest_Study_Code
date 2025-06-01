6ffc1b1a234392c8dea9e3089f07b60d6f766e24
minseok kwon <kwonminseok7364@naver.com>
2025-06-01 22:27:32 +0900

    6/1

--- week_3/4_thu/키패드 누르기.py
+++ week_3/4_thu/키패드 누르기.py
@@ -1,24 +1,37 @@
-answer = ""
+def solution(numbers, hand):
+    answer = ""
+    keypad = {
+        1: (0, 0), 2: (0, 1), 3: (0, 2),
+        4: (1, 0), 5: (1, 1), 6: (1, 2),
+        7: (2, 0), 8: (2, 1), 9: (2, 2),
+        '*': (3, 0), 0: (3, 1), '#': (3, 2)
+    }
 
-num = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-button = {1:0,2:0,3:1,4:0,5:0,
-          6:0,7:0,8:0,9:0}
+    left_pos = keypad['*']
+    right_pos = keypad['#']
 
-for i in button:
-    print("button의 i값입니다.",i)
-    if button[i] == 1:
-        print("ㅇㅇㅇㅇㅇㅇ",i)
-
-
-print(button[3])
-# for i in num: 뭐ㅓ라고?
-#     a = 0
-#     if button[i] == 1:
-
-#     if i == 1 or i == 4 or i == 7:
-#         button[i] = 1
-#         answer += "L"
-#     elif i == 3 or i == 6 or i == 9:
-#         button[i] = 2
-#         answer += "R"
-#     elif (i -  
+    for num in numbers:
+        if num in [1, 4, 7]:
+            answer += "L"
+            left_pos = keypad[num]
+        elif num in [3, 6, 9]:
+            answer += "R"
+            right_pos = keypad[num]
+        else:
+            target = keypad[num]
+            l_dist = abs(left_pos[0] - target[0]) + abs(left_pos[1] - target[1])
+            r_dist = abs(right_pos[0] - target[0]) + abs(right_pos[1] - target[1])
+            if l_dist < r_dist:
+                answer += "L"
+                left_pos = target
+            elif r_dist < l_dist:
+                answer += "R"
+                right_pos = target
+            else:
+                if hand == "right":
+                    answer += "R"
+                    right_pos = target
+                else:
+                    answer += "L"
+                    left_pos = target
+    return answer
---
+++ week_3/5_fri/최고의 집합.py
@@ -0,0 +1,15 @@
+def solution(n, s):
+    n1 = [1,2,3,4,5,6,7,8,9]
+    n2 = [1,2,3,4,5,6,7,8,9]
+    label = 
+    for i in n1:
+        for j in n2:
+            if i + j == s:
+
+    
+    
+    # answer = []
+    return k
+
+print(solution(2, 8))
+
